f982cd12e7e1d3aa6073730383f6160d24968996
yanhuan3 <502160565@qq.com>
2024-07-23 15:01:56 +0800

    修改播放卡顿

---
+++ Harmony-arkts-movie-music-app-ui/entry/src/main/ets/common/constant.ts
@@ -0,0 +1,8 @@
+// export const HOST:string = 'http://254a2y1767.qicp.vip';// 外网映射地址，一般只有晚上才开机，开机之后才能使用，如需调试请联系
+export const HOST: string = 'http://192.168.0.5:5001'; // 内网地址
+export const USER_DATA: string = 'USER_DATA';
+export const MUSIC_PLAYER = 'MUSIC_PLAYER';// 音乐播放器
+export const MUSIC_MODEL = 'MUSIC_MODEL';// 音乐实例对象
+export const MUSIC_STORAGE = 'MUSIC_STORAGE';// 音乐仓库
+export const MUSIC_SEARCH = 'MUSIC_SEARCH';// 音乐搜查
+export const FAVORITE_MUSIC = 'FAVORITE_MUSIC';// 收藏音乐
